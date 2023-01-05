afbc23913d5cc0f76da0a70833c042408f687652
George O Connor <george@georgestools.com>
2023-01-05 14:17:01 +0000

    f

--- test.py
+++
@@ -1,5 +0,0 @@
-from src.sharepoint_stuff import getCTX, uploadFile
-
-ctx = getCTX("https://ioedub.sharepoint.com/sites/AttendanceTest/", "goconnor@instituteofeducation.ie", "Welcome2IOE!")
-
-uploadFile(ctx, "test.docx", "test.docx", "Shared Documents")
